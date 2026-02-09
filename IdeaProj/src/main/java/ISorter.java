2c5bb51a8e8ffd42db7f9d872e7ed87245af392f
URETZ <u-beresnev2006@mail.ru>
2026-02-09 19:00:56 +0700

    ISorter

--- IdeaProj/src/main/java/ISorter.java
+++ IdeaProj/src/main/java/ISorter.java
@@ -1,2 +1,8 @@
+import java.util.List;
 public interface ISorter {
+    public void Sort(List<String> Files);
+    public void getInt();
+    public void getFloat();
+    public void getString();
+
 }
